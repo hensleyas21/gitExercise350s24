5851fcb952f68283a9959a5b1fdeeb91e8950bf8
AustinHensley <hensleas21@icloud.com>
2024-02-19 14:37:03 -0500

    branching

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi!");
         System.out.println("Ethan Kesterholt is mean");
-        System.out.println(".idea/blah");
+        System.out.println("blah");
+        System.out.println("does this work???");
     }
 }
